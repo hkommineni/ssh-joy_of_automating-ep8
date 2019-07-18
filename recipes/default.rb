f4043cbd60683ab7ff798d0488e4b1c5d73dc38f
Harish Kommineni <komhar01@CSGICORP.COM>
2019-07-18 12:25:32 -0500

    Working so far with basic kitchen verify

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,8 +6,18 @@
 
 directory '/root/.ssh' do
   recursive true
+  mode '0600'
 end
 
 file '/root/.ssh/config' do
-  content 'MaxSessions 250'
+  mode '0600'
+  content %{
+Host *
+    ForwardAgent no
+    Port 22
+Host build-server
+    hostname 10.0.0.1
+    user builder
+    identityfile ~/.ssh/keyfile
+  }
 end
--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,14 +1,8 @@
 require 'spec_helper'
 
 describe 'ssh::default' do
-  # Serverspec examples can be found at
-  # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
-  end
-
   describe file('/root/.ssh/config') do
-    its(:content) { should match(/MaxSessions 250/) }
-    # its(:content) { should match(/MaxStartups 250/) }
+    it { should exist }
+    its(:content) { should match(/user builder/) }
   end
 end
